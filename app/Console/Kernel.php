aa52a5d464b3e900534cd6fd9b876cc83a830453
cavang124 <ngocbich100999@gmail.com>
2021-09-18 20:34:28 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('word:day')->daily();
+        $schedule->command('word:day')->everyTwoMinutes();
     }
 
     /**
